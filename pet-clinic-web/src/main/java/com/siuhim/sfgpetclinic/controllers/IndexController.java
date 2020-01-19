2437f87dbfbde95f1741c9b838cff1117d95c3c1
himhimlo <himhimlo126@gmail.com>
2020-01-19 10:52:41 +0800

    Remove empty RequestMapping

--- pet-clinic-web/src/main/java/com/siuhim/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/siuhim/sfgpetclinic/controllers/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class IndexController {
 
-    @RequestMapping({"", "/"})
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
