1f7da28dcebf39e969bfa7be920b9e92f8beb2ec
himhimlo <himhimlo126@gmail.com>
2020-01-25 15:26:23 +0800

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/siuhim/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.siuhim.sfgpetclinic.services;
+
+import com.siuhim.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/siuhim/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package com.siuhim.sfgpetclinic.services.map;
+
+import com.siuhim.sfgpetclinic.model.PetType;
+import com.siuhim.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
