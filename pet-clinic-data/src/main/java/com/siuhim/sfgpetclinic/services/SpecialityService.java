8c6ddf4a979a535088b6987642f0836c5737ffd4
himhimlo <himhimlo126@gmail.com>
2020-01-26 10:11:10 +0800

    Added Speciality map service impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/siuhim/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.siuhim.sfgpetclinic.services;
+
+import com.siuhim.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/siuhim/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package com.siuhim.sfgpetclinic.services.map;
+
+import com.siuhim.sfgpetclinic.model.Speciality;
+import com.siuhim.sfgpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality> implements SpecialityService {
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+}
