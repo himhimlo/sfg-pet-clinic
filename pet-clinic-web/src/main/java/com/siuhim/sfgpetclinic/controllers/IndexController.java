51adaebb82f8535bc00d5d16149030e646aa6d46
himhimlo <himhimlo126@gmail.com>
2020-01-26 12:08:23 +0800

    Fixed broken links. Closes #45

--- pet-clinic-web/src/main/java/com/siuhim/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/siuhim/sfgpetclinic/controllers/IndexController.java
@@ -1,11 +1,23 @@
 package com.siuhim.sfgpetclinic.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class IndexController {
 
     @RequestMapping({"", "/"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/vets.html")
+    public String vetsRedirect() {
+        return "redirect:/vets";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+        return "notimplemented";
     }
 }
