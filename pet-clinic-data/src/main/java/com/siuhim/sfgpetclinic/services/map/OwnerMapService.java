c4b8aaf2773d020e8f76c9fe1c28fc1f1bbd2dcf
himhimlo <himhimlo126@gmail.com>
2020-06-20 17:59:16 +0800

    Added tests. Closes #49

--- pet-clinic-data/src/main/java/com/siuhim/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/siuhim/sfgpetclinic/services/map/OwnerMapService.java
@@ -68,6 +68,10 @@ public class OwnerMapService extends AbstractMapService<Owner> implements OwnerS
 
     @Override
     public Owner findByLastName(String lastName) {
-        return null;
+        return findAll()
+                .stream()
+                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+                .findFirst()
+                .orElse(null);
     }
 }
